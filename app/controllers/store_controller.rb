868aa840b6475ee74de022e710424574ef84eb70
irisgyq <gouyingqiyanxuan321@gmail.com>
2018-02-07 16:24:37 -0500

    revise bugs

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,6 @@ class StoreController < ApplicationController
 	include CurrentCart
 	before_action :set_cart
   def index
-  	@products=Product.order(:title)
+    @products=Product.order(:title)
   end
 end
--- db/migrate/20180107021522_combine_items_in_cart.rb
+++ db/migrate/20180107021522_combine_items_in_cart.rb
@@ -14,17 +14,17 @@ class CombineItemsInCart < ActiveRecord::Migration[5.1]
   end
 
   def down
-  	LineItem.where("quantity>1").each do |line_item|
-  		line_item.quantity.times do
-  			LineItem.create(
-  				cart_id: line_item.cart.id,
-  				product_id: line_item.product_id,
-  				quantity: 1
-  				)
-  		end
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart.id,
+          product_id: line_item.product_id,
+          quantity: 1
+          )
+      end
 
-  		line_item.destroy
-  	end
+      line_item.destroy
+    end
   end
 
 end
