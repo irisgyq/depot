966533a938a09bd247aa9cc9568ab1cb0127a3d2
irisgyq <gouyingqiyanxuan321@gmail.com>
2018-02-08 00:48:34 -0500

    ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
 	include CurrentCart
 	before_action :set_cart
-  def index
-    @products=Product.order(:title)
-  end
+    
+    def index
+      @products = Product.order(:title)
+    end
 end
