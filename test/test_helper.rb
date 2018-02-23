06969e3d2d9eff512f64a6b757dc1c26687166e0
irisgyq <gouyingqiyanxuan321@gmail.com>
2018-02-23 01:04:55 -0500

    log in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,22 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+	fixtures :all
+end
+
+class ActionDispatch::IntegrationTest
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  def login_as(user)
+  	post login_url, params: { name: user.name, password: 'secret' }
+  end
+
+  def logout
+  	delete logout_url
+  end
+
+  def setup
+  	login_as users(:one)
+  end
 
   # Add more helper methods to be used by all tests here...
 end
