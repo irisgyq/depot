f26610b63378a762d8f8077786c453d13567051a
irisgyq <gouyingqiyanxuan321@gmail.com>
2018-03-01 02:09:18 -0500

    webpack-config-js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app/javascript/packs/pay_type.jsx',
+  output: {
+    path: __dirname + '/app/javascript/PayTypeSelector',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
